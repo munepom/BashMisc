bc078c7fd4a547b6f9914a61314ecbb87bcb1c51
munepuyo <munepuyo@users.noreply.github.com>
2014-09-04 10:35:09 +0900

    Delete getDaysOfMonth.sh

--- getDaysOfMonth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-### get days of month
-function getDaysOfMonth() {
-        local YEAR=$1
-        local MONTH=$2
-        local DAYS=$(date -d "${YEAR}/${MONTH}/1 + 1 month - 1 day" +"%d")
-        echo ${DAYS}
-}
