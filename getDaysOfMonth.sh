f3bed7b5a4337f4823bf4c305489a45d571bfaa7
munepuyo <munepuyo@users.noreply.github.com>
2014-09-04 10:31:52 +0900

    Update getDaysOfMonth.sh

--- getDaysOfMonth.sh
+++ getDaysOfMonth.sh
@@ -4,6 +4,6 @@
 function getDaysOfMonth() {
         local YEAR=$1
         local MONTH=$2
-        local DATE=$(date -d "${YEAR}/${MONTH}/1 + 1 month - 1 day" +"%d")
-        echo ${DATE}
+        local DAYS=$(date -d "${YEAR}/${MONTH}/1 + 1 month - 1 day" +"%d")
+        echo ${DAYS}
 }
